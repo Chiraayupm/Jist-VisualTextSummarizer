84807a90d7088db58210e2d51ed645aa14563bb1
Parth Cheulkar <pscheulkar12@gmail.com>
2022-03-09 12:23:25 +0530

    add base html template and cdn

--- summarizer/main/urls.py
+++ summarizer/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import main,index
 
 urlpatterns = [
-    path('', main.main, name=""),
+    # path('', main.main, name=""),
+    path('', index.index, name="index"),
 ]
